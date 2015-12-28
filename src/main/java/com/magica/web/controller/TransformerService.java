1595938a0d30ec0cdc13a25b4a314eff1e7af13f
Nataraj Gnanavadivel <Nataraj.Gnanavadivel@gmail.com>
2015-12-28 16:43:47 -0500

    Code has been cleaned up as per latest code conventions.

--- src/main/java/com/magica/web/controller/TransformerService.java
+++ src/main/java/com/magica/web/controller/TransformerService.java
@@ -6,7 +6,6 @@ import java.io.IOException;
 import org.apache.commons.io.FileUtils;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.DefaultMessageCodesResolver.Format;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,81 +18,84 @@ import com.codemagic.magica.AutomatorRequest;
 @RequestMapping("/")
 public class TransformerService {
 
-	private String workspaceDir = "C:/Users/gnanavad/Documents/stash-all-in-one/magica-gui/workspace";
+   private String workspaceDir = "C:/Users/gnanavad/Documents/stash-all-in-one/magica-gui/workspace";
 
-	@RequestMapping(value = "services/transform", method = RequestMethod.POST, produces = {
-			MediaType.APPLICATION_JSON_VALUE })
-	public @ResponseBody String transform(@RequestParam("service") String service,
-			@RequestParam("src_schema") String srcSchema,
-			@RequestParam("tgt_schema") String tgtSchema,
-			@RequestParam("mapping") String mapping, @RequestParam("src_data") String srcData,
-			@RequestParam("src_fmt") String srcFmt, @RequestParam("tgt_fmt") String tgtFmt) throws Exception {
+   @RequestMapping(value = "services/transform",
+                   method = RequestMethod.POST,
+                   produces = { MediaType.APPLICATION_JSON_VALUE })
+   public @ResponseBody String transform(@RequestParam("service") String service,
+                                         @RequestParam("src_schema") String srcSchema,
+                                         @RequestParam("tgt_schema") String tgtSchema,
+                                         @RequestParam("mapping") String mapping,
+                                         @RequestParam("src_data") String srcData,
+                                         @RequestParam("src_fmt") String srcFmt,
+                                         @RequestParam("tgt_fmt") String tgtFmt) throws Exception {
 
-		initServiceWorkspace(service);
+      initServiceWorkspace(service);
 
-		String sourceSchemaFile = resourcesDirectory(service) + "/source.xsd";
-		String tgtSchemaFile = resourcesDirectory(service) + "/target.xsd";
-		String mappingFile = resourcesDirectory(service) + "/mapping.properties";
+      String sourceSchemaFile = resourcesDirectory(service) + "/source.xsd";
+      String tgtSchemaFile = resourcesDirectory(service) + "/target.xsd";
+      String mappingFile = resourcesDirectory(service) + "/mapping.properties";
 
-		writeAsFile(sourceSchemaFile, srcSchema);
-		writeAsFile(tgtSchemaFile, tgtSchema);
-		writeAsFile(mappingFile, mapping);
+      writeAsFile(sourceSchemaFile, srcSchema);
+      writeAsFile(tgtSchemaFile, tgtSchema);
+      writeAsFile(mappingFile, mapping);
 
-		AutomatorRequest req = new AutomatorRequest();
-		req.srcFormat = AutomatorRequest.FORMAT.valueOf(srcFmt);
-		req.destFormat = AutomatorRequest.FORMAT.valueOf(tgtFmt);
-		req.service = service;
-		req.srcSchemaName = "source.xsd";
-		req.tgtSchemaName = "target.xsd";
-		req.srcRoot = "DEAL";
-		req.tgtRoot = "Loan";
-		req.baseDir = serviceDirectory(service);
-		req.sourceInstance = srcData;
-		return new Automator().automate(req);
+      AutomatorRequest req = new AutomatorRequest();
+      req.srcFormat = AutomatorRequest.FORMAT.valueOf(srcFmt);
+      req.destFormat = AutomatorRequest.FORMAT.valueOf(tgtFmt);
+      req.service = service;
+      req.srcSchemaName = "source.xsd";
+      req.tgtSchemaName = "target.xsd";
+      req.srcRoot = "DEAL";
+      req.tgtRoot = "Loan";
+      req.baseDir = serviceDirectory(service);
+      req.sourceInstance = srcData;
+      return new Automator().automate(req);
 
-	}
+   }
 
-	private void initServiceWorkspace(String service) {
-		createDirectory(serviceDirectory(service));
-		createDirectory(resourcesDirectory(service));
-		createDirectory(generatedDirectory(service));
-		createDirectory(compiledDirectory(service));
-		createDirectory(jarDirectory(service));
-	}
+   private String compiledDirectory(String service) {
+      return serviceDirectory(service) + "/compiled";
+   }
 
-	private void writeAsFile(String file, String contents) throws IOException {
-		FileUtils.writeStringToFile(new File(file), contents);
-	}
+   private void createDirectory(String dir) {
+      if (dir != null) {
+         File file = new File(dir);
+         if (!file.exists()) {
+            file.mkdirs();
+         } else { // watch out this logic.
+            file.delete();
+            file.mkdirs();
+         }
+      }
+   }
 
-	private void createDirectory(String dir) {
-		if (dir != null) {
-			File file = new File(dir);
-			if (!file.exists()) {
-				file.mkdirs();
-			} else { // watch out this logic.
-				file.delete();
-				file.mkdirs();
-			}
-		}
-	}
+   private String generatedDirectory(String service) {
+      return serviceDirectory(service) + "/generated";
+   }
 
-	private String serviceDirectory(String service) {
-		return workspaceDir + "/" + service;
-	}
+   private void initServiceWorkspace(String service) {
+      createDirectory(serviceDirectory(service));
+      createDirectory(resourcesDirectory(service));
+      createDirectory(generatedDirectory(service));
+      createDirectory(compiledDirectory(service));
+      createDirectory(jarDirectory(service));
+   }
 
-	private String resourcesDirectory(String service) {
-		return serviceDirectory(service) + "/resources";
-	}
+   private String jarDirectory(String service) {
+      return serviceDirectory(service) + "/jar";
+   }
 
-	private String generatedDirectory(String service) {
-		return serviceDirectory(service) + "/generated";
-	}
+   private String resourcesDirectory(String service) {
+      return serviceDirectory(service) + "/resources";
+   }
 
-	private String compiledDirectory(String service) {
-		return serviceDirectory(service) + "/compiled";
-	}
+   private String serviceDirectory(String service) {
+      return workspaceDir + "/" + service;
+   }
 
-	private String jarDirectory(String service) {
-		return serviceDirectory(service) + "/jar";
-	}
+   private void writeAsFile(String file, String contents) throws IOException {
+      FileUtils.writeStringToFile(new File(file), contents);
+   }
 }
